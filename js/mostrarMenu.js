22448ce201bc745a15540e9cec94fc508bb3c590
Mariaxbc <me28carvalho@gmail.com>
2022-11-01 19:44:05 -0300

    Atualizando

--- js/mostrarMenu.js
+++ js/mostrarMenu.js
@@ -6,11 +6,19 @@ function mostrarMenu() {
         menu.style.display = "flex";
         icone.classList.remove("fa-bars");
         icone.classList.add("fa-times");
+<<<<<<< HEAD
+        // Muda para times
+=======
         // Muda para times 
+>>>>>>> 008c25cd45092c45111d01612d21aeacf6ae7836
     } else {
         menu.style.display = "none";
         icone.classList.remove("fa-times");
         icone.classList.add("fa-bars");
+<<<<<<< HEAD
+        // Muda para Bars
+=======
         // Muda para bars
+>>>>>>> 008c25cd45092c45111d01612d21aeacf6ae7836
     }
 }
